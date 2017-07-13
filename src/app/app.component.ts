c252573af536d8b687c1794f88c006f62cf82e6c
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2017-07-12 21:03:34 -0400

    Fail lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'test-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
