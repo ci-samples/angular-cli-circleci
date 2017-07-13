9fdbb43b2ccbadf122bdf33eaabb7732a53fd0fe
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2017-07-12 21:00:19 -0400

    Revert "Fail build"
    This reverts commit 5f9cc2bad6091e02a971037f033f4240643417f8.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent1 {
+export class AppComponent {
   title = 'app works!';
 }
